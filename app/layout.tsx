41a7cd9a21b2a84095a82940ea46c1a9d7d0a740
Ayush_Shukla <ayush_shukla@Ninja-ITs-MacBook-Air.local>
2024-04-04 22:48:48 +0530

    Login Modal added

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import ClientOnly from "./components/ClientOnly";
 import Modal from "./components/modals/Modal";
 import RegisterModal from "./components/modals/RegisterModal";
 import ToasterProvider from "./providers/ToasterProvider";
+import LoginModal from "./components/modals/LoginModal";
 
 const inter = Inter({ subsets: ["latin"] });
 const font = Nunito({
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
+          <LoginModal/>
           <RegisterModal />
           {/* <Modal isOpen={true} title="Hello World" actionLabel="Submit" /> */}
           <Navbar />
