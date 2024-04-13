67b9db25a86a79541b876247a03498ba8424e1cc
Ayush4141 <ashukla4141@gmail.com>
2024-04-13 11:02:47 +0530

    reservations added

--- app/components/listings/ListingHead.tsx
+++ app/components/listings/ListingHead.tsx
@@ -1,11 +1,13 @@
 import useCountries from "@/app/hooks/useCountries";
 import { SafeUser } from "@/app/types";
 import Heading from "../Heading";
+import Image from "next/image";
+import HeartButton from "../HeartButton";
 
 interface ListingHeadProps {
   title: string;
   locationValue: string;
-  imageSrc?: string;
+  imageSrc: string;
   id?: string;
   currentUser?: SafeUser | null;
 }
@@ -15,13 +17,27 @@ const ListingHead: React.FC<ListingHeadProps> = ({
   locationValue,
   imageSrc,
   id,
-  currentUser}
-) => {
+  currentUser,
+}) => {
   const { getByValue } = useCountries();
   const location = getByValue(locationValue);
   return (
     <>
-      <Heading title={title} subtitle={`${location?.region}, ${location?.label}`} />
+      <Heading
+        title={title}
+        subtitle={`${location?.region}, ${location?.label}`}
+      />
+      <div className="w-full h-[60vh] overflow-hidden rounded-xl relative">
+        <Image
+          src={imageSrc}
+          alt="Image"
+          fill
+          className="object-cover w-full"
+        />
+        <div className="absolute top-5 right-5">
+          <HeartButton listingId={id} currentUser={currentUser} />
+        </div>
+      </div>
     </>
   );
 };
--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -2,31 +2,54 @@
 
 import Container from "@/app/components/Container";
 import ListingHead from "@/app/components/listings/ListingHead";
+import ListingInfo from "@/app/components/listings/ListingInfo";
 import { categories } from "@/app/components/navbar/Categories";
+import useLoginModal from "@/app/hooks/useLoginModal";
 import { SafeListings, SafeUser } from "@/app/types";
 import { Reservation } from "@prisma/client";
+import { eachDayOfInterval } from "date-fns";
+import { useRouter } from "next/navigation";
 import { useMemo } from "react";
+
+const initialDateRange = {
+  startDate: new Date(),
+  endDate: new Date(),
+  key: "selection",
+};
 
 interface ListingClientPops {
   reservation?: Reservation[];
-  listing?: SafeListings & {
+  listing: SafeListings & {
     user: SafeUser;
   };
   currentUser?: SafeUser | null;
 }
 
 const ListingClient: React.FC<ListingClientPops> = ({
-  reservation,
+  reservations = [],
   listing,
   currentUser,
 }) => {
+  const loginModal = useLoginModal();
+  const router = useRouter();
 
-  
+  const disabledDates = useMemo(() => {
+    let dates: Date[] = [];
+    reservations.forEach((reservation: any) => {
+      const range = eachDayOfInterval({
+        start: new Date(reservation.startDate),
+        end: new Date(reservation.endDate),
+      });
+
+      dates = [...dates,...range];
+    });
+    return dates
+  }, [reservations]);
+
 
   const category = useMemo(() => {
     return categories.find((item) => item.label === listing?.category);
   }, [listing?.category]);
-  console.log("hiii",listing)
 
   return (
     <Container>
@@ -38,6 +61,17 @@ const ListingClient: React.FC<ListingClientPops> = ({
           id={listing?.id}
           currentUser={currentUser}
         />
+        <div className="grid grid-col-1 md:grid-cols-7 md:gap-10 mt-6">
+          <ListingInfo
+            user={listing?.user}
+            category={category}
+            description={listing?.category}
+            coomCount={listing?.roomCount}
+            guestCount={listing?.guestCount}
+            bathroomCount={listing?.bathroomCount}
+            locationValue={listing?.locationValue}
+          />
+        </div>
       </div>
     </Container>
   );
